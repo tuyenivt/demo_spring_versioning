0334ad69c712d8a8e84d1baa2490ef06129c0c42
Tuyen Le Chau <tuyenivt@gmail.com>
2020-09-19 21:26:53 +0700

    initial

---
+++ src/main/java/com/example/versioning/VersioningApplication.java
@@ -0,0 +1,13 @@
+package com.example.versioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersioningApplication.class, args);
+	}
+
+}
